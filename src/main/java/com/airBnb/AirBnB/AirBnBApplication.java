75e6e24bb49546e825ad418680ec8bfde63ac1a6
Sagar Goyal <sagargoyal2015@gmail.com>
2025-10-04 16:13:12 +0530

    project initialization

---
+++ src/main/java/com/airBnb/AirBnB/AirBnBApplication.java
@@ -0,0 +1,13 @@
+package com.airBnb.AirBnB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airBnb/AirBnB/AirBnBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airBnb.AirBnB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
